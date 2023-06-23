c0617e465b96a7aa9d9ea4437febd1f05f21d3f0
liciniopsj <licinio.psj@gmail.com>
2023-06-23 17:57:39 -0300

    Req03 OK / Flaked

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,32 @@
-# Req 3
+import csv
+from src.models.ingredient import Ingredient
+from src.models.dish import Dish
+
+
 class MenuData:
-    def __init__(self, source_path: str) -> None:
-        pass
+    def __init__(self, source_path: str):
+        self.load_file(source_path)
+
+    def load_file(self, source_path: str):
+        with open(source_path, 'r', encoding='utf-8') as file:
+            reader = csv.DictReader(file)
+            self.dishes = set()
+
+            for row in reader:
+                dish_name = row['dish']
+                dish_price = float(row['price'])
+                ingredient_name = row['ingredient']
+                recipe_amount = int(row['recipe_amount'])
+
+                dish = self.get_or_create_dish(dish_name, dish_price)
+                ingredient = Ingredient(ingredient_name)
+                dish.add_ingredient_dependency(ingredient, recipe_amount)
+
+    def get_or_create_dish(self, name: str, price: float) -> Dish:
+        for dish in self.dishes:
+            if dish.name == name and dish.price == price:
+                return dish
+
+        new_dish = Dish(name, price)
+        self.dishes.add(new_dish)
+        return new_dish
